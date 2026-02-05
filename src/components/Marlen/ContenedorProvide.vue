cd4ddfa35215d82b46f106ee5d5cf45780909874
alvarezmarlen <marlenjosefalvarez@gmail.com>
2026-02-05 06:41:36 +0100

    funcionalidad de props

--- src/components/Marlen/ContenedorProvide.vue
+++ src/components/Marlen/ContenedorProvide.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="contenedor">
-        <p>Componente Intermedio</p>
+        <p>Comunicación a Larga Distancia: Provide e Inject</p>
         <NietoInject/>
     </div>
 </template>
@@ -12,11 +12,17 @@ import NietoInject from './NietoInject.vue'
 <style scoped>
 .contenedor {
     margin: 2rem auto;
-    width: 50%;
+    width: 60%;
     color: black;
     background-color: #e0faf2;
     text-align: center;
     font-size: 1.5rem;
     border-radius: 1.5rem;
+    padding: 2rem 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    border: 1px solid #b2ebda;
 }
 </style>
